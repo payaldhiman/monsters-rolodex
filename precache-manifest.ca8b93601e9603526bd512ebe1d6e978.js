4912883f91367f6342c240aa7e9aa4b5ea0090ea
Payal Dhiman <payaldhiman@outlook.com>
2020-04-21 23:45:54 -0400

    Updates

---
+++ precache-manifest.ca8b93601e9603526bd512ebe1d6e978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f830bb638e8d1b9a50bf912c31a82ed3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "66a508aff6ef42f786b0",
+    "url": "/monsters-rolodex/static/css/main.8f26bf84.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a508aff6ef42f786b0",
+    "url": "/monsters-rolodex/static/js/main.470734c4.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
